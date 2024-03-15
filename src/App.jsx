e102eb76e44676088d7b9af16bcc288695e9148b
Tayyaba <tayyaba.afzal@gmail.com>
2024-03-15 20:05:15 +0000

    links added to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import MainNav from '/src/components/MainNav/index.jsx'
+import Hero from '/src/components/MainHero/index.jsx'
 import axios from 'axios'
 
 
@@ -10,38 +10,49 @@ function App() {
 
   useEffect(() => {
     // Set the API endpoint URL
-const apiUrl = 'https://www.reed.co.uk/api/1.0/search';
+    const apiUrl = 'https://www.reed.co.uk/api/1.0/search';
 
-// Set the API key
-const apiKey = '2461cb12-4726-4b4c-8409-75aed7f5fb9d';
+    // Set the API key
+    const apiKey = '2461cb12-4726-4b4c-8409-75aed7f5fb9d';
 
-// Set the search parameters
-const searchParams = {
-  keywords: 'accountant',
-  locationName: 'London',
-  distanceFromLocation: '15',
-  // Add more search parameters as needed
-};
+    // Set the search parameters
+    const searchParams = {
+      keywords: 'accountant',
+      locationName: 'London',
+      distanceFromLocation: '15',
+      // Add more search parameters as needed
+    };
 
-// Set the proxy server URL
-const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    // Set the proxy server URL
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 
-// Make the API request using Axios
-axios.get(proxyUrl + apiUrl, {
-  params: searchParams,
-  headers: {
-    'Authorization': `Basic ${btoa(apiKey + ':')}`,
-  },
-})
-.then((response) => {
-  // Handle the response data
-  console.log(response.data);
-})
-.catch((error) => {
-  // Handle any errors
-  console.error(error);
-});
+    // Make the API request using Axios
+    axios.get(proxyUrl + apiUrl, {
+      params: searchParams,
+      headers: {
+        'Authorization': `Basic ${btoa(apiKey + ':')}`,
+      },
+    })
+      .then((response) => {
+        // Handle the response data
+        console.log(response.data);
+      })
+      .catch((error) => {
+        // Handle any errors
+        console.error(error);
+      });
   }, []);
+
+  return (
+    <>
+      <MainNav />
+
+      <Hero />
+    </>
+
+  );
 }
+
+
 
 export default App
--- src/components/MainNav/index.jsx
+++ src/components/MainNav/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import tailwindConfig from '../../../tailwind.config';
 
 function Navbar() {
 
