f6ce91dcea6702a5150d0fd5d0a3f2aa9477c145
Tayyaba <tayyaba.afzal@gmail.com>
2024-03-15 14:47:40 +0000

    new folder made in components. logo added to nav component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import axios from 'axios'
 
+
 function App() {
   const [count, setCount] = useState(0);
 
---
+++ src/components/MainHero/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+
+function Hero() {
+  return (
+<div class= "hero" >
+  <div className= "hero-text text-center text-red text-3xl">
+<h1> Jobify</h1>
+  </div>
+  </div>
+  );
+}
+
+export default Hero
