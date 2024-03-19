dd42d84f914d0cf3e106eeae380f492f0af83bb7
Allan <daniel.joyce@icloud.com>
2024-03-19 19:24:11 +0000

    made changes to postcss and tailwind.config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
+import autoprefixer from "autoprefixer";
+import tailwind from "tailwindcss";
+import tailwindConfig from "./tailwind.config.js";
+
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [autoprefixer, tailwind(tailwindConfig)],
+
 }
