dc30465d4a8dd389c5d37cd7d2194fd876d8b303
tayyabaifzal <151195085+tayyabaifzal@users.noreply.github.com>
2024-03-19 19:50:37 +0000

    tailwind components added for navbar, hero and footer. App.jsk file e… (#7)
    * tailwind components added for navbar, hero and footer. App.jsk file editted with links of components
    
    * links removed

---
+++ src/components/MainSearch/index.jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+
+function Search() {
+
+    return (
+        <div className="flex justify-center items-center">
+            </div>
+    );
+}
+
+export default Search
