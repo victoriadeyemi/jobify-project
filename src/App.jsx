cdcde486826cdb3238e8e655c8910e99ac497c44
Victoria Adeyemi <151677799+victoriadeyemi@users.noreply.github.com>
2024-03-19 21:37:49 +0000

    search results card

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import MainNav from '/src/components/MainNav/index.jsx'
 import MainHero from '/src/components/MainHero/index.jsx'
 import MainFooter from '/src/components/MainFooter/index.jsx'
 import MainSearch from '/src/components/MainSearch/index.jsx'
+import MainSearchResults from '/src/components/MainSearchResults/index.jsx'
 import axios from 'axios'
 
 
@@ -51,11 +52,12 @@ function App() {
 
       <MainHero />
 
-      <MainFooter />
-
       <MainSearch/>
 
+      <MainSearchResults/>
 
+      <MainFooter />
+      
     </>
 
   );
