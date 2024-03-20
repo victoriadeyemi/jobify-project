8ef49ad181cf5ee400e2e5293ca0e86b15ce0e1f
Victoria Adeyemi <151677799+victoriadeyemi@users.noreply.github.com>
2024-03-20 23:16:44 +0000

    react router installed

--- src/components/MainSearchResults/index.jsx
+++ src/components/MainSearchResults/index.jsx
@@ -1,16 +1,15 @@
 import react from 'react';
 
-function MainSearchResults ({jobTitle, jobLocation, jobMinSalary, jobPosted, jobMaxSalary, jobLink, closeDate}) {
+function MainSearchResults () {
     return (
         <div className="searchResults">
             
-<a href={jobLink} class="block max-w-md p-6 bg-white border border-cyan-600 rounded-lg shadow hover:bg-cyan-800 dark:bg-cyan-600 dark:border-cyan-800 dark:hover:bg-cyan-800">
+<a href="#" class="block max-w-sm p-6 bg-white border border-cyan-600 rounded-lg shadow hover:bg-cyan-800 dark:bg-cyan-600 dark:border-cyan-800 dark:hover:bg-cyan-800">
 
-<h5 class="mb-2 text-2xl font-bold tracking-tight text-cyan-900 dark:text-white">{jobTitle}</h5>
-<p class="font-light text-cyan-100 dark:text-cyan-100">Date Posted: {jobPosted}</p>
-<p class="font-light text-cyan-100 dark:text-cyan-100">Close Date: {closeDate}</p>
-<p class="font-bold text-cyan-100 dark:text-cyan-20">£{jobMinSalary} - £{jobMaxSalary}</p>
-<p class="font-normal text-cyan-100 dark:text-cyan-100">{jobLocation}</p>
+<h5 class="mb-2 text-2xl font-bold tracking-tight text-cyan-900 dark:text-white">Software developer</h5>
+<p class="font-light text-cyan-100 dark:text-cyan-100">job posted 2days ago</p>
+<p class="font-bold text-cyan-100 dark:text-cyan-20">£29,000 - £35000 per annum</p>
+<p class="font-normal text-cyan-100 dark:text-cyan-100">location Wolverhampton</p>
 <p class="font-normal text-cyan-100 dark:text-cyan-100">See More...</p>
 </a>
 
