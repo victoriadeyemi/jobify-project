6dfa3dd4b35e4301fe78f6ff5c88daad955037e7
Victoria Adeyemi <151677799+victoriadeyemi@users.noreply.github.com>
2024-03-21 10:01:07 +0000

    job search result page

--- src/components/JobSearchResult/index.jsx
+++ src/components/JobSearchResult/index.jsx
@@ -1,4 +1,13 @@
 import React from "react";
 
 function JobSearchResult() {
+    return (
+        <div>
+        <h1>JobSearchResult</h1>
+        </div>
+    );
+    }
+
+
+    export default JobSearchResult;
     
