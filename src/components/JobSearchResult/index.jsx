19a9ba8d414331c2abd362914b0ee7e7abe556fc
Victoria Adeyemi <victoriadeyemi1038@gmail.com>
2024-03-21 17:22:46 +0000

    Va modalfix (#21)
    * job search result page
    
    * fixed className, footer height, main hero height.

--- src/components/JobSearchResult/index.jsx
+++ src/components/JobSearchResult/index.jsx
@@ -1,4 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import MainSearchResults from "../MainSearchResults";
 
 function JobSearchResult() {
-    
+    return (
+        <div>
+        <h1>JobSearchResult</h1>
+        </div>
+    );
+    }
+
+    export default JobSearchResult
